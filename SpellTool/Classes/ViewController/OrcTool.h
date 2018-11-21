3a6b02e4de82873fd0ba70d832461cf68c0b6d44
yy_nest <yy_nest@126.com>
2018-11-21 23:13:13 +0800

    ORC

---
+++ SpellTool/Classes/ViewController/OrcTool.h
@@ -0,0 +1,17 @@
+//
+//  OrcTool.h
+//  SpellTool
+//
+//  Created by Fedora on 2018/11/21.
+//  Copyright © 2018 Fedora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OrcTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SpellTool/Classes/ViewController/OrcTool.m
@@ -0,0 +1,13 @@
+//
+//  OrcTool.m
+//  SpellTool
+//
+//  Created by Fedora on 2018/11/21.
+//  Copyright © 2018 Fedora. All rights reserved.
+//
+
+#import "OrcTool.h"
+
+@implementation OrcTool
+
+@end
